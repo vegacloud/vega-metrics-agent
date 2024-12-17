41564694b48207934ba4e773fb209af602b70bad
Brent Clements <brent@vegacloud.io>
2024-12-17 10:52:38 -0800

    Various fixes

--- test/resources/buildkindtestcluster.sh
+++ test/resources/buildkindtestcluster.sh
@@ -1,6 +1,7 @@
 curl -Lo ./kind https://kind.sigs.k8s.io/dl/latest/kind-linux-amd64
 chmod +x ./kind
-./kind create cluster --name test-cluster --config kind-config.yaml --image=kindest/node:v1.30.0
+./kind create cluster --name testcluster-1 --config kind-config.yaml --image=kindest/node:v1.30.0
 echo "Sleeping 20 seconds to let things settle down)
 sleep(20)
 kubectl apply -f ./test-resources.yaml
+kubectl debug -n test-metrics test-pod --image=busybox -it --target=main-container
